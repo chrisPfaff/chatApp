a9e5a6bfdbefc161979455e006ea5f9f8986c600
Chris Pfaff <chrispfaff10@gmail.com>
2018-02-20 18:01:50 -0500

    Changed Chat Component and removed Tailwind

---
+++ src/components/Chat.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Form from './Form'
+
+class Chat extends Component {
+
+  render() {
+    return (
+      <div className="chatBox">
+        <Form/>
+      </div>
+    );
+  }
+
+}
+
+export default Chat;
